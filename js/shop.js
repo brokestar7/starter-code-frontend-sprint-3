37b119551842d4a40e91db1b26ec2029dbc6a98c
brokestar7 <82914463+brokestar7@users.noreply.github.com>
2022-04-27 12:55:13 +0200

    agregando repositorio sprint 3

--- js/shop.js
+++ js/shop.js
@@ -67,6 +67,8 @@ var total = 0;
 function buy(id) {
   // 1. Loop for to the array products to get the item to add to cart
   // 2. Add found product to the cartList array
+  
+  
 }
 
 // Exercise 2
