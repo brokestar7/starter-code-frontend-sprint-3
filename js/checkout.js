2373b7e196d4e0b5848a0ea19f7f5c87e8e72666
brokestar7 <82914463+brokestar7@users.noreply.github.com>
2022-05-17 12:53:31 +0200

    cambios en las validaciones

--- js/checkout.js
+++ js/checkout.js
@@ -11,4 +11,15 @@ var errorPhone = document.getElementById("errorPhone");
 // Exercise 6
 function validate(event) {
   // Validate fields entered by the user: name, phone, password, and email
+  // event.preventDefault();
+  let name = document.getElementById("fName").value;
+  if(name.length <=3 ){
+    name.classList.add("is-invalid");
+    return
+  }
+  else{
+    name.classList.add("is-valid");
+    return
+  }
+
 }
