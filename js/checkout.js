2597d854d3b5d40ed106960490b9c73874d62a36
brokestar7 <82914463+brokestar7@users.noreply.github.com>
2022-05-18 09:53:23 +0200

    correccion validaciones

--- js/checkout.js
+++ js/checkout.js
@@ -9,22 +9,44 @@ var errorName = document.getElementById("errorName");
 var errorPhone = document.getElementById("errorPhone");
 
 // Exercise 6
-function validate(event) {
+function validateCheckout() {
+  error = 0;
   // Validate fields entered by the user: name, phone, password, and email
   // event.preventDefault();  
   let name = document.getElementById("fName").value;
   let formN = document.getElementById("fName");
-  if(name.length <3 ){
-    formN.classList.remove("is-valid");
+  let email = document.getElementById("fEmail").value;
+  let formE = document.getElementById("fEmail");
+  let phone = document.getElementById("fPhone").value;
+  let formP = document.getElementById("fPhone");
+  let password = document.getElementById("fPassword").value;
+  let formPassword = document.getElementById("fPassword");
+  if(name == '' || name < 3 ){
     formN.classList.add("is-invalid");
-    return;
+    error++;
   }
-  else{
+  if(email == "" || email < 3){
+    formE.classList.add("is-invalid");
+  }
+  if(phone == "" || phone < 9){
+    formP.classList.add("is-invalid");
+  }
+  if(password == "" || password < 4){
+    formPassword.classList.add("is-invalid");
+  }
+
+  
+  
+  if(error>0){
     formN.classList.remove("is-invalid");
-    formN.classList.add("is-valid");
-    return;
+    formE.classList.remove("is-invalid");
+    formP.classList.remove("is-invalid");
+    formPassword.classList.remove("is-invalid");
+    return false;
+  }else{
+
+      return true;
   }
-  // ---------
 
   
   
