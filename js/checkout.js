193bc8cf3b535fc76292209856ab93816aa86dd7
brokestar7 <82914463+brokestar7@users.noreply.github.com>
2022-05-19 09:33:40 +0200

    correccion form

--- js/checkout.js
+++ js/checkout.js
@@ -10,44 +10,56 @@ var errorPhone = document.getElementById("errorPhone");
 
 // Exercise 6
 function validateCheckout(event) {
-  error = 0;
   // Validate fields entered by the user: name, phone, password, and email
+  error = 0;
   event.preventDefault();  
-  let name = document.getElementById("fName").value;
   let formN = document.getElementById("fName");
-  let lName = document.getElementById("fLastN").value;
   let formLName = document.getElementById("fLastN");
-  let address = document.getElementById("fAddress").value;
   let formAddress = document.getElementById("fAddress");
-  let email = document.getElementById("fEmail").value;
   let formE = document.getElementById("fEmail");
-  let phone = document.getElementById("fPhone").value;
   let formP = document.getElementById("fPhone");
-  let password = document.getElementById("fPassword").value;
   let formPassword = document.getElementById("fPassword");
-  if(name == "" || name.length < 3 ){
+  if(formN.value == "" || formN.value.length < 3 ){
     formN.classList.add("is-invalid");
     error++;
   }
-  if(lName == "" || lName.length < 3 ){
+  else{
+    formN.classList.remove("is-invalid");
+  }
+  if(formLName.value == "" || formLName.value.length < 3 ){
     formLName.classList.add("is-invalid");
     error++;
   }
-  if(address == "" || address.length < 3 ){
+  else{
+    formLName.classList.remove("is-invalid");
+  }
+  if(formAddress.value == "" || formAddress.value.length < 3 ){
     formAddress.classList.add("is-invalid");
     error++;
   }
-  if(email == "" || email.length < 3){
+  else{
+    formAddress.classList.remove("is-invalid");
+  }
+  if(formE.value == "" || formE.value.length < 3){
     formE.classList.add("is-invalid");
     error++;
   }
-  if(phone == "" || phone.length != 9){
+  else{
+    formE.classList.remove("is-invalid");
+  }
+  if(formP.value == "" || formP.value.length != 9){
     formP.classList.add("is-invalid");
     error++;
   }
-  if(password == "" || password.length < 4){
+  else{
+    formP.classList.remove("is-invalid");
+  }
+  if(formPassword.value == "" || formPassword.value.length < 4){
     formPassword.classList.add("is-invalid");
     error++;
+  }
+  else{
+    formPassword.classList.remove("is-invalid");
   }
 
   
